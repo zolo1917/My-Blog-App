8505db23e55510fbfbc58b486052872d808f3fb8
zolo1917 <adwaith_venkatesh@msn.com>
2022-11-01 22:50:57 +0530

    update blog layout to include other aspects of the blog posts

--- blog-app-ui/apps/zolo-blog/src/app/app.module.ts
+++ blog-app-ui/apps/zolo-blog/src/app/app.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCommonModule } from '@angular/material/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
+// time to see if this makes the thin
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatButtonModule,
+    MatCommonModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatTabsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
